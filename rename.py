020c7426cb820eb98e075e19b0f61f299b0421a1
soulpastwk <soul.past@gmail.com>
2025-01-11 15:06:29 +0500

    1

--- rename.py
+++ rename.py
@@ -1,9 +1,8 @@
 import os
-
 # данный скрипт удаляет часть названия папок и файла во всех вложенных в директорию файлах
-# в данном примере это - [BOOMINFO.RU] 
+# в данном примере это - [BOOMINFO.RU]
 
-# ЗАМЕНИ ТУТ - выдели всемте со скобками и нажми Ctrl+H - [BOOMINFO.RU] 
+# ЗАМЕНИ ТУТ - выдели всемте со скобками и нажми Ctrl+H - [BOOMINFO.RU]
 
 # Укажите путь к корневой директории
 root_dir = r"D:\Курсы Милла"
@@ -12,21 +11,23 @@ def remove_prefix_in_directory(path):
     for dirpath, dirnames, filenames in os.walk(path, topdown=False):
         # Обрабатываем файлы
         for filename in filenames:
-            if filename.startswith("[BOOMINFO.RU] "):
+            if filename.startswith("[BOOMINFO.RU]"):
                 old_file_path = os.path.join(dirpath, filename)
-                new_file_name = filename.replace("[BOOMINFO.RU] ", "", 1)
+                new_file_name = filename.replace("[BOOMINFO.RU]", "", 1)
                 new_file_path = os.path.join(dirpath, new_file_name)
                 os.rename(old_file_path, new_file_path)
                 print(f"Файл переименован: {old_file_path} -> {new_file_path}")
         
         # Обрабатываем папки
         for dirname in dirnames:
-            if dirname.startswith("[BOOMINFO.RU] "):
+            if dirname.startswith("[BOOMINFO.RU]"):
                 old_dir_path = os.path.join(dirpath, dirname)
-                new_dir_name = dirname.replace("[BOOMINFO.RU] ", "", 1)
+                new_dir_name = dirname.replace("[BOOMINFO.RU]", "", 1)
                 new_dir_path = os.path.join(dirpath, new_dir_name)
                 os.rename(old_dir_path, new_dir_path)
                 print(f"Папка переименована: {old_dir_path} -> {new_dir_path}")
 
 # Запускаем функцию
 remove_prefix_in_directory(root_dir)
+# Теперь внизу во вкладке TERMINAL нажми стрелочку вверх и Enter
+# Если ничего не появилось то напечатай .\rename.py
