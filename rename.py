3ffd6583f95d8b29e2ad0e5bf2234e6aecf0f10f
soulpastwk <soul.past@gmail.com>
2025-01-11 19:22:04 +0500

    1

--- rename.py
+++ rename.py
@@ -1,8 +1,8 @@
 import os
 # данный скрипт удаляет часть названия папок и файла во всех вложенных в директорию файлах
-# в данном примере это - [BOOMINFO.RU]
+# в данном примере это - [SW.BAND]
 
-# ЗАМЕНИ ТУТ - выдели всемте со скобками и нажми Ctrl+H - [BOOMINFO.RU]
+# ЗАМЕНИ ТУТ - выдели всемте со скобками и нажми Ctrl+H - [SW.BAND]
 
 # Укажите путь к корневой директории
 root_dir = r"D:\Курсы Милла"
@@ -11,18 +11,18 @@ def remove_prefix_in_directory(path):
     for dirpath, dirnames, filenames in os.walk(path, topdown=False):
         # Обрабатываем файлы
         for filename in filenames:
-            if filename.startswith("[BOOMINFO.RU]"):
+            if filename.startswith("[SW.BAND]"):
                 old_file_path = os.path.join(dirpath, filename)
-                new_file_name = filename.replace("[BOOMINFO.RU]", "", 1)
+                new_file_name = filename.replace("[SW.BAND]", "", 1)
                 new_file_path = os.path.join(dirpath, new_file_name)
                 os.rename(old_file_path, new_file_path)
                 print(f"Файл переименован: {old_file_path} -> {new_file_path}")
         
         # Обрабатываем папки
         for dirname in dirnames:
-            if dirname.startswith("[BOOMINFO.RU]"):
+            if dirname.startswith("[SW.BAND]"):
                 old_dir_path = os.path.join(dirpath, dirname)
-                new_dir_name = dirname.replace("[BOOMINFO.RU]", "", 1)
+                new_dir_name = dirname.replace("[SW.BAND]", "", 1)
                 new_dir_path = os.path.join(dirpath, new_dir_name)
                 os.rename(old_dir_path, new_dir_path)
                 print(f"Папка переименована: {old_dir_path} -> {new_dir_path}")
